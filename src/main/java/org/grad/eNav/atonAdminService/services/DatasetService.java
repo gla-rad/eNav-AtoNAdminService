b347a4103f3837cd8a6a1ca3b5ece5d398d3ddb7
Nikolaos Vastardis <Nikolaos.Vastardis@gla-rad.org>
2025-12-04 13:26:33 +0000

    Fixing more content type headers to strings

--- src/main/java/org/grad/eNav/atonAdminService/services/DatasetService.java
+++ src/main/java/org/grad/eNav/atonAdminService/services/DatasetService.java
@@ -302,7 +302,7 @@ public class DatasetService {
 
         // Publish the cancelled dataset to the deleted channel
         this.s201RemovalChannel.send(MessageBuilder.withPayload(result)
-                .setHeader(MessageHeaders.CONTENT_TYPE, SECOM_DataProductType.S201)
+                .setHeader(MessageHeaders.CONTENT_TYPE, SECOM_DataProductType.S201.name())
                 .setHeader("operation", DatasetOperation.CANCELLED)
                 .build());
 
@@ -334,7 +334,7 @@ public class DatasetService {
 
         // Publish the updated dataset to the deleted channel
         this.s201RemovalChannel.send(MessageBuilder.withPayload(result)
-                .setHeader(MessageHeaders.CONTENT_TYPE, SECOM_DataProductType.S201)
+                .setHeader(MessageHeaders.CONTENT_TYPE, SECOM_DataProductType.S201.name())
                 .setHeader("operation", DatasetOperation.DELETED)
                 .build());
 
@@ -416,7 +416,7 @@ public class DatasetService {
                                 result.getUuid());
                         // Publish the updated dataset to the publication channel
                         this.s201PublicationChannel.send(MessageBuilder.withPayload(result)
-                                .setHeader(MessageHeaders.CONTENT_TYPE, SECOM_DataProductType.S201)
+                                .setHeader(MessageHeaders.CONTENT_TYPE, SECOM_DataProductType.S201.name())
                                 .setHeader("operation", result.isNew() ?
                                         DatasetOperation.CREATED : DatasetOperation.UPDATED)
                                 .build());
